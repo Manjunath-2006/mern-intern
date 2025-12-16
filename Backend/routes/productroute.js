452c64699fe5010ba69dc0f00fef78bb7f57d3b0
Manjunath-2006 <smm90669@gmail.com>
2025-12-16 09:48:08 +0530

    Delete Backend directory

--- Backend/routes/productroute.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const { createproduct } = require("../Controllers/productController");
-
-const route = express.Router();
-
-route.post("/", createproduct);
-
-module.exports = route;
